5e19f2bdbe6d84ee07d9006e56b360bf5904c7a1
Joaquín Chemile <joaquinchemile@gmail.com>
2019-07-01 21:39:33 -0300

    Create a general proyect for the classes scripts

--- utn/ayed/tests/Ejemplo05/main.cpp
+++ utn/ayed/tests/Ejemplo05/main.cpp
@@ -1,29 +1,10 @@
 #include <iostream>
+#include "vectores.h"
 
 using namespace std;
+
 int main()
 {
-    char vectorPrueba[5] = "pera";
-    char vectorResultante[5] = "";
-
-    for(int i = 0; i < 4; i++){
-        cout << vectorPrueba[i] << endl;
-    }
-
-    cout << "" << endl;
-
-    for(int i = 4; i >= 0; i--){
-        cout << vectorPrueba[i] << endl;
-    }
-
-
-    for(int i = 0; i <= 4; i++){
-            vectorResultante[i] = vectorPrueba[4-i];
-    }
-
-    cout << "Resultante" << endl;
-        for(int i = 0; i <= 4; i++){
-        cout << vectorResultante[i] << endl;
-    }
+    prueba_vectores();
 
 }
