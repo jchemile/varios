34fdda33b6f7b429890bae2c57b53e854aaee3b2
Joaquín Chemile <joaquinchemile@gmail.com>
2019-07-16 21:09:47 -0300

    Added the material for the video 1

--- gaurav_gaur/functional_scala/src/main/scala/main.scala
+++ gaurav_gaur/functional_scala/src/main/scala/main.scala
@@ -1,3 +1,10 @@
+import tail_recursion._
+
 object main {
+
+  def main(args: Array[String]): Unit = {
+
+    println(sum_tail_rec(99999999, 1))
+  }
 
 }
--- gaurav_gaur/functional_scala/src/main/scala/tail_recursion.scala
+++ gaurav_gaur/functional_scala/src/main/scala/tail_recursion.scala
@@ -1,3 +1,23 @@
+import scala.annotation._
+
 object tail_recursion {
 
+  //Recursive algorithm without tail recursion
+  def gdc(a: Int, b: Int): Int = {
+    if (b == 0) a
+    else gdc(b, a % b)
+  }
+
+
+  def sum(num: Int): Int = {
+    if (num == 1) 1
+    else sum(num - 1) + num
+  }
+
+  //tail recursion
+  @tailrec
+  def sum_tail_rec(num: Int, res: Int): Int = {
+    if(num == 1) res
+    else sum_tail_rec(num - 1, res + num)
+  }
 }
