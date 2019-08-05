7683b4bf8d0980b219e33a51a6167b07e716789e
joaquinchemile <joaquin@nubimetrics.com>
2019-08-05 11:43:27 -0300

    Reorder Notebooks and utilities.

--- dev_inside_you/scala_tutorial/src/ClosuresAndCurrying.scala
+++ dev_inside_you/scala_tutorial/src/ClosuresAndCurrying.scala
@@ -9,7 +9,25 @@ object ClosuresAndCurrying {
   }
 
   def code(args: Array[String]): Unit = {
-    println("Hello World")
+
+    def closeTerm(freeVariable: Int)(boundVariable: Int): Int =
+      freeVariable + boundVariable
+
+    def uncurriedClosedTerm(freeVariable: Int, boundVariable: Int): Int =
+      freeVariable + boundVariable
+
+    println(uncurriedClosedTerm(freeVariable =   7, 1))
+    println(uncurriedClosedTerm(freeVariable =  10, 2))
+    println(uncurriedClosedTerm(freeVariable =  20, 3))
+    println(uncurriedClosedTerm(freeVariable =  30, 4))
+
+    println()
+
+    println(closeTerm(freeVariable =  7)(0))
+    println(closeTerm(freeVariable = 10)(1))
+    println(closeTerm(freeVariable = 20)(2))
+    println(closeTerm(freeVariable = 30)(3))
+
   }
 
 }
--- utilities/scala_utilities/src/main/scala/scala_collections.scala
+++ utilities/scala_utilities/src/main/scala/scala_collections.scala
@@ -16,6 +16,19 @@ object scala_collections {
     val L3 = (L1, L2).zipped.map(_ :: _)
     print(L3)
 
+    //Flat Map
+    def isPrime(n:Int)= (2 until n) forall(n% _ != 0)
+    val n = 7
+    (1 until n) flatMap(i => (1 until i) map (j => (i,j))) filter (pair => isPrime(pair._1 + pair._2))
+
+    //List Flattern
+    //Original list:<br>(a, list(b,c,d))
+    //Output Require:
+    //(a,b)
+    //(a,c)
+    //(a,d)
+    val example = ('a', List('b','c','d'))
+    val res = example._2.map((example._1,_))
 
   }
 
