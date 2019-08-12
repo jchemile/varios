cb771400424020288d9fd12d9039bd156f5fa763
Joaquín Chemile <joaquinchemile@gmail.com>
2019-08-12 01:44:02 -0300

    Add new excercies

---
+++ dev_inside_you/scala_tutorial/src/practices/ObjectOrientationValue.scala
@@ -0,0 +1,16 @@
+package practices
+
+object ObjectOrientationValue {
+  def main(args: Array[String]): Unit = {
+    println("*" * 50)
+
+    code(args)
+
+    println("*" * 50)
+  }
+
+  def code(args: Array[String]): Unit = {
+    print("Hello World")
+  }
+
+}
---
+++ dev_inside_you/scala_tutorial/src/project/clock/TwoDimensionalString.scala
@@ -0,0 +1,47 @@
+package project.clock
+
+final case class TwoDimensionalString(
+                                        top:    String,
+                                         middle: String,
+                                         bottom: String
+                                       ) {
+  require(
+    requirement = doAllRowsHaveSameLength,
+    message = "All rows must have the same lenght!"
+  )
+
+  def +(that: TwoDimensionalString): TwoDimensionalString =
+      TwoDimensionalString(
+        top = this.top       + that.top,
+        middle = this.middle + that.middle,
+        bottom = this.bottom + that.bottom
+      )
+
+    private[this] def doAllRowsHaveSameLength: Boolean =
+      top.length == middle.length && middle.length == bottom.length
+
+    def show(): Unit = {
+      println(rendered)
+    }
+
+    private[this] def rendered: String =
+      top      + "\n" +
+        middle + "\n" +
+        bottom
+}
+
+object TwoDimensionalString {
+  val QuestionMarks: TwoDimensionalString =
+    TwoDimensionalString(
+      top    = "   ",
+      middle = " ? ",
+      bottom = " ? "
+    )
+  val Empty: TwoDimensionalString =
+    TwoDimensionalString(
+      top    = "",
+      middle = "",
+      bottom = ""
+    )
+}
+
