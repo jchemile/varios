236425f0481662f148d5e41c32dee79dbfebe723
joaquinchemile <joaquin@nubimetrics.com>
2019-08-16 18:29:05 -0300

    Update notebooks

---
+++ utilities/scala_utilities/src/main/scala/permutations.scala
@@ -0,0 +1,26 @@
+object permutations {
+
+  def main(args: Array[String]): Unit = {
+    println("*"*131)
+
+    code(args)
+
+    println("*"*131)
+  }
+
+  def code(args: Array[String]): Unit = {
+
+    "ABC".permutations.toList
+
+    val bca:Char => Char = {
+      case 'A' => 'B'
+      case 'B' => 'C'
+      case 'C' => 'A'
+    }
+
+    "ABC".map(bca)
+
+
+  }
+
+}
--- utilities/scala_utilities/src/main/scala/scala_collections.scala
+++ utilities/scala_utilities/src/main/scala/scala_collections.scala
@@ -4,7 +4,7 @@ object scala_collections {
 
     println("*"*150)
 
-    code(args)
+    map_practise(args)
 
     println("*"*150)
 
@@ -42,9 +42,86 @@ object scala_collections {
     */
   }
 
+  def list_from_option_vector(args: Array[String]): Unit = {
+
+    //Objective: Get the total count of the element.
+    //Ex: If I look for Z the method should return 4
+
+    val x = Vector(
+      Some(List(("x",2))),
+      Some(List(("x",2),
+        ("y",3))),
+      None,
+      Some(List(("x",2),
+        ("z",2))),
+      Some(List(("x",2),("z",2))),
+      None)
+
+    val x2 = x.flatMap{
+      case Some(l) => l.filter(_._1=="x").map(_._2)
+      case None => List(0)
+    }.sum
+
+    println(x2)
+
+  }
+
+  def list_practise(args: Array[String]): Unit = {
+
+    val a = List(13, 17, 99, 54)
+
+    def last[T](xs: List[T]): T =xs match {
+      case List() => throw new Error("last of empty list")
+      case List(x) => x
+      case y :: ys => last(ys)
+    }
+
+    println(last(a))
+
+    def init[T](xs: List[T]): List[T] = xs match {
+      case List() => throw new Error("init of empty list")
+      case List() => List()
+      case y :: ys => y :: init(ys)
+    }
+
+    println(init(a))
+
+
+    def reverse[T](xs: List[T]): List[T] = xs match {
+      case List() => xs
+      case y :: ys => reverse(ys) ++ List(y)
+    }
+
+    def removeAt[T](n: Int, xs: List[Int]) = (xs take n) ::: (xs drop n+1)
+
+  }
+
+  def map_practise(args: Array[String]): Unit = {
+    val romanNumerals = Map("I" -> 1, "V" -> 5, "X" -> 10)
+    val capitalOfCountry = Map("US" -> "Washington", "Switzerland" -> "Bern")
+
+    capitalOfCountry get "US"
+
+    def showCapital(country: String) = capitalOfCountry.get(country) match {
+      case Some(capital) => capital
+      case None => "missing data"
+    }
+
+    showCapital("US")
+
+    val fruit = List("apple", "pear", "orange", "pineapple")
+
+    fruit sortWith(_.length < _.length)
+
+    fruit.sorted
+
+    fruit groupBy(_.head)
+
+  }
+
   def code (args: Array[String]): Unit = {
 
-    println("Hello")
+
   }
 
 }
