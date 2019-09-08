df163fdb7656fa365c38da09e320f68f8d525dce
Joaquín Chemile <joaquinchemile@gmail.com>
2019-09-08 01:58:14 -0300

    Strings algorithns

--- lucid_programming/algorithm/source/practices/strings/string_processing.py
+++ lucid_programming/algorithm/source/practices/strings/string_processing.py
@@ -3,21 +3,131 @@ def next_number(s):
     i = 0
     while i < len(s):
         count = 1
-        while i + 1 < len(s) and s[i] == s[i+1]:
-            i+= 1
+        while i + 1 < len(s) and s[i] == s[i + 1]:
+            i += 1
             count += 1
         result.append(str(count) + s[i])
         i += 1
     return ''.join(result)
 
-s = "1"
-n = 4
 
-for i in range(n):
-    s = next_number(s)
-    print(s)
+def spreadsheet_encode_column(col_str):
+    num = 0
+    count = len(col_str) - 1
+    for s in col_str:
+        num += 26**count * (ord(s) - ord("A") + 1)
+        count -= 1
+    return num
 
 
+def is_palindrome(s):
+    i = 0
+    j = len(s) - 1
+
+    while i < j:
+        while not s[i].isalnum() and i < j:
+            i += 1
+        while not s[j].isalnum() and i < j:
+            j -= 1
+
+        if s[i].lower() != s[j].lower():
+            return False
+
+        i += 1
+        j -= 1
+    return True
 
 
+def is_anagram(s1, s2):
+    ht = dict()
 
+    if len(s1) != len(s2):
+        return False
+
+    for i in s1:
+        if i in ht:
+            ht[i] += 1
+        else:
+            ht[i] = 1
+
+    for i in s2:
+        if i in ht:
+            ht[i] -= 1
+        else:
+            ht[i] = 1
+
+    for i in ht:
+        if ht[i] != 0:
+            return False
+    return True
+
+
+def is_palin_prem(input_str):
+    input_str = input_str.replace(" ", "")
+    input_str = input_str.lower()
+
+    d = dict()
+
+    for i in input_str:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+
+    odd_count = 0
+    for k, v in d.items():
+        if v % 2 != 0 and odd_count == 0:
+            odd_count += 1
+        elif v % 2 != 0 and odd_count != 0:
+            return False
+    return True
+
+
+def is_perm_1(str1, str2):
+    str_1 = str1.lower()
+    str_2 = str2.lower()
+
+    if len(str1) != len(str2):
+        return False
+
+    str_1 = ''.join(sorted(str_1))
+    str_2 = ''.join(sorted(str_2))
+
+    n = len(str_1)
+    for i in range(n):
+        if str_1[i] != str_2[i]:
+            return False
+    return True
+
+
+def is_perm_2(str1, str2):
+
+    str_1 = str1.lower()
+    str_2 = str2.lower()
+
+    if len(str_1) != len(str_2):
+        return False
+
+    d = dict()
+
+    for i in str_1:
+        if i in d:
+            d[i] -= 1
+        else:
+            d[i] = 1
+    for i in str_2:
+        if i in d:
+            d[i] -= 1
+        else:
+            d[i] = 1
+    return all(value == 0 for value in d.values())
+
+A = "god"
+B = "Dog"
+C = "ZARO"
+D = "TuVO"
+
+print(is_perm_1(A, B))
+print(is_perm_1(C, D))
+print(is_perm_2(A, B))
+print(is_perm_2(C, D))
