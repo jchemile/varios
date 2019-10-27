e8373eeef8fa535426432bda1185020cf3adc20d
Joaquín Chemile <joaquinchemile@gmail.com>
2019-10-27 12:33:39 -0300

    Linked list practice

--- programming_knowledge/data-structures/main.cpp
+++ programming_knowledge/data-structures/main.cpp
@@ -12,16 +12,20 @@ node* head;
 
 int main()
 {
+    cout << "Begin" << endl;
     head = NULL;
+
+        node* second_node = new node;
+    second_node -> data = 2;
+    second_node -> link = head ->link;
+    head = second_node;
 
     node* first_node = new node;
     first_node -> data = 1;
     first_node -> link = NULL;
 
-    node* second_node = new node;
-    second_node -> data = 2;
-    second_node -> link = head ->link;
-    head = second_node;
+
+
 
     cout << first_node -> data << endl;
     cout << first_node -> link << endl;
