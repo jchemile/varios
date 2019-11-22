1412f55c5872ed9cd5505cd038f3d669b7d77d59
Joaquín Chemile <joaquinchemile@gmail.com>
2019-11-22 00:42:47 -0300

    Fix pyspark implementation

--- utilities/python_utilities/main/src/spark/TestDataFrame.py
+++ utilities/python_utilities/main/src/spark/TestDataFrame.py
@@ -1,3 +1,8 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.master("local").appName("spark-python-utilites").getOrCreate()
+test = spark.createDataFrame(["10","11","13"], "string").toDF("age")
+
+test.show()
+
+print("fin")
