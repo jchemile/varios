fb70b154a001fb3b6c604669b9bd6b76f13661e4
Joaquín Chemile <jchemile@users.noreply.github.com>
2020-03-24 23:15:18 -0300

    Serializacion

---
+++ pildoras_informaticas/java/src/acceso_ficheros/Serializacion.java
@@ -0,0 +1,103 @@
+package acceso_ficheros;
+
+import java.io.*;
+import java.util.*;
+
+public class Serializacion {
+
+    public static void main(String[] args) {
+
+        Administrador jefe = new Administrador("Juan", 80000, 2005,12,15);
+        jefe.setIncentivo(5000);
+
+        Empleado[] personal = new Empleado[3];
+
+        personal[0] = jefe;
+        personal[1] = new Empleado("Ana", 25000,2008,10,1);
+        personal[2] = new Empleado("Luis", 18000,2012,9,15);
+
+        try{
+
+            ObjectOutputStream escribiendo_fichero = new ObjectOutputStream(new FileOutputStream("src/acceso_ficheros/empleado.dat"));
+            escribiendo_fichero.writeObject(personal);
+            escribiendo_fichero.close();
+
+            ObjectInputStream recuperando_fichero = new ObjectInputStream(new FileInputStream("src/acceso_ficheros/empleado.dat"));
+
+            Empleado[] personal_recuperado = (Empleado[]) recuperando_fichero.readObject();
+
+            recuperando_fichero.close();
+
+            for( Empleado e: personal_recuperado){
+                System.out.println(e);
+            }
+
+        } catch (Exception e){
+
+        }
+
+    }
+
+
+
+
+    static class Empleado implements Serializable {
+
+        public Empleado(String n, double s, int agno, int mes, int dia){
+            nombre = n;
+            sueldo = s;
+            GregorianCalendar calendario = new GregorianCalendar(agno, mes-1, dia);
+            fechaContrato = calendario.getTime();
+        }
+
+        public String getNombre(){
+            return nombre;
+        }
+
+        public double getSueldo(){
+            return sueldo;
+        }
+
+        public Date getFechaContrato(){
+            return fechaContrato;
+        }
+
+        public void subirSueldo(double porcentaje){
+            double aumento = sueldo*porcentaje/100;
+            sueldo+=aumento;
+        }
+
+        public String toString(){
+            return "El nombre es = " + nombre +", y su sueldo es = "+ sueldo+ ", fecha de contrato= " + fechaContrato;
+        }
+
+        private String nombre;
+        private Double sueldo;
+        private Date fechaContrato;
+
+    }
+
+    static class Administrador extends Empleado {
+
+        public Administrador(String n, double s, int agno, int mes, int dia){
+            super(n,s,agno,mes,dia);
+            incentivo = 0;
+        }
+
+        public double getSueldo(){
+            double sueldoBase = super.getSueldo();
+            return sueldoBase + incentivo;
+        }
+
+        public void setIncentivo(double b){
+            incentivo = b;
+        }
+
+        public String toString(){
+            return super.toString()+" Incentivo = " + incentivo;
+        }
+
+
+        private double incentivo;
+    }
+}
