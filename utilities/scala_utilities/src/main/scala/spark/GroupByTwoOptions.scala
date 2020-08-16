ce29a4a4c803f8581f847a9dc531a1d936280977
Joaquín Chemile <jchemile@users.noreply.github.com>
2020-08-16 04:23:09 -0300

    grouping and sorting with two options

---
+++ utilities/scala_utilities/src/main/scala/spark/GroupByTwoOptions.scala
@@ -0,0 +1,41 @@
+package spark
+
+import spark.TestSession.sparkSession
+
+object GroupByTwoOptions extends App with SparkWrapper {
+  import sparkSession.implicits._
+
+  val df = Seq(
+    (1, 1, "m10", "t22"),
+    (1, 2, "m10", "t22"),
+    (1, 3, "m11", "t22"),
+    (1, 4, "m11", "t22"),
+    (1, 5, "m10", "t22"),
+    (1, 6, "m10", "t22"),
+    (1, 7, "m10", "t22"),
+    (1, 8, "m11", "t22"),
+    (1, 9, "m10", "t22"),
+    (1, 10, "m10", "t22"),
+    (2, 1, "m10", "t22"),
+    (2, 2, "m11", "t22"),
+    (2, 3, "m10", "t22"),
+    (2, 4, "m10", "t22"),
+    (2, 5, "m10", "t22"),
+    (2, 9, "m10", "t22"),
+    (2, 10, "m11", "t22"),
+    (3, 4, "m10", "t22"),
+    (3, 5, "m11", "t22"),
+    (3, 6, "m10", "t22"),
+    (3, 7, "m10", "t22"),
+    (3, 8, "m10", "t22"),
+    (3, 9, "m11", "t22"),
+    (3, 10, "m10", "t22")).toDF("org_id", "rule_id", "period_id", "base_id")
+
+
+  df.show(50, false)
+
+  val df2 = df.orderBy("org_id", "rule_id")
+
+  df2.show(50, false)
+
+}
